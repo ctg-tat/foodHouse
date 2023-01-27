991778217beabccbc53beacf7b0212e78dcb17c2
ctg <kamilayanaul@mail.ru>
2023-01-27 14:41:56 +0300

    Add filter and hover effects

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,106 +1,64 @@
-import img1 from "../../assets/emojione_green-salad.svg"
-import img2 from "../../assets/fluent_food-pizza-24-filled.svg"
-import img3 from "../../assets/mdi_hamburger.svg"
-import img4 from "../../assets/noto-v1_french-fries.svg"
-import img5 from "../../assets/fluent_food-pizza-24-filled.svg"
-import img6 from "../../assets/tabler_discount-2.svg"
+import { useEffect, useState } from "react";
 
-import img11 from "../../assets/item.png"
-import img12 from "../../assets/item__image.png"
-import img13 from "../../assets/item__image3.png"
-import img14 from "../../assets/item__image4.png"
-import img15 from "../../assets/item.png"
-
-import Category from "../Category/Category"
-import Product from "../Product/Product"
-
-const categories = [
-    {
-        src: img1,
-        name: "Салаты",
-    },
-    {
-        src: img2,
-        name: "Напитки",
-    },
-    {
-        src: img3,
-        name: "Бургеры",
-    },
-    {
-        src: img4,
-        name: "Закуски",
-    },
-    {
-        src: img5,
-        name: "Пицца",
-    },
-    {
-        src: img6,
-        name: "Акции",
-    },
-];
-
-const products = [
-    {
-        src: img11,
-        name: "Салат “Греческий”"
-    },
-    {
-        src: img12,
-        name: "Салат “Греческий”"
-    },
-    {
-        src: img13,
-        name: "Салат “Тропический”"
-    },
-    {
-        src: img14,
-        name: "Салат “Японка”"
-    },
-    {
-        src: img15,
-        name: "Салат “Собери Сам”"
-    },
-    {
-        src: img12,
-        name: "Салат “Цезарь”"
-    },
-    {
-        src: img13,
-        name: "Салат “Оливье”"
-    },
-    {
-        src: img14,
-        name: "Салат “Летний”"
-    },
-];
+import Product from "../Product/Product";
+import { CATEGORIES } from "../../data/categories";
+import { PRODUCTS } from "../../data/products";
 
 const Menu = () => {
-    return(
-        <section className="menu">
-            <div className="section-title">
-                Наше Меню
-                <div className="line"></div>
-            </div>
+  const [products, setProducts] = useState(PRODUCTS);
 
-            <div className="categories">
-                {
-                    categories.map((category) => <Category url={category.src} name={category.name}/>)
-                }
-            </div>
+  const [selectedCategory, setSelectedCategory] = useState("none");
 
-            <div className="products">
-                {
-                    products.map((product) => <Product url={product.src} name={product.name}/>)
-                }
-            </div>
+  const onClickCategoryHandle = (type) => {
+    setSelectedCategory(type);
+  };
 
-            <button className="button button-more">
-                Показать ещё
-            </button>
-        </section>
-    )
-}
+  useEffect(() => {
+    setProducts((prevState) => {
+      prevState = [...prevState];
+
+      prevState = PRODUCTS.filter(
+        (product) => product.type === selectedCategory
+      );
+
+      if (selectedCategory === "none") {
+        prevState = PRODUCTS;
+      }
+
+      return prevState;
+    });
+  }, [selectedCategory]);
+
+  return (
+    <section className="menu">
+      <div className="section-title">
+        Наше Меню
+        <div className="line"></div>
+      </div>
+
+      <div className="categories">
+        {CATEGORIES.map((category) => {
+          return (
+            <div
+              onClick={() => onClickCategoryHandle(category.type)}
+              className="category"
+            >
+                {category.src}
+              <p className="category-name">{category.name}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="products">
+        {products.map((product) => (
+          <Product url={product.src} name={product.name} />
+        ))}
+      </div>
+
+      <button className="button button-more">Показать ещё</button>
+    </section>
+  );
+};
 
 export default Menu;
---
+++ src/data/products.js
@@ -0,0 +1,84 @@
+import img11 from "../assets/item.png"
+import img12 from "../assets/item__image.png"
+import img13 from "../assets/item__image3.png"
+import img14 from "../assets/item__image4.png"
+import img15 from "../assets/item.png"
+import img16 from "../assets/d404a990c2a4703139cc6bcf7786ec0f-320x320.jpg"
+import img17 from "../assets/63208.png"
+import img18 from "../assets/burger.jpg"
+import img19 from "../assets/hvostiki_iz_shprot.jpg"
+import img20 from "../assets/pizza.png"
+import img21 from "../assets/2.jpg"
+
+export const PRODUCTS = [
+    {
+        src: img11,
+        name: "Салат “Греческий”",
+        type: "salad"
+    },
+    {
+        src: img12,
+        name: "Салат “Греческий”",
+        type: 'salad'
+    },
+    {
+        src: img13,
+        name: "Салат “Тропический”",
+        type: 'salad'
+    },
+    {
+        src: img14,
+        name: "Салат “Японка”",
+        type: "salad"
+    },
+    {
+        src: img15,
+        name: "Салат “Собери Сам”",
+        type: "salad"
+    },
+    {
+        src: img12,
+        name: "Салат “Цезарь”",
+        type: "salad",
+    },
+    {
+        src: img13,
+        name: "Салат “Оливье”",
+        type: "salad",
+    },
+    {
+        src: img14,
+        name: "Салат “Летний”",
+        type: "salad",
+    },
+    {
+        src: img16,
+        name: "Chupa-chups",
+        type: "drinks",
+    },
+    {
+        src: img17,
+        name: "Azlano",
+        type: "drinks",
+    },
+    {
+        src: img18,
+        name: "Сочный бургер",
+        type: "burger",
+    },
+    {
+        src: img19,
+        name: "Хвостик из шпрот",
+        type: "snacks",
+    },
+    {
+        src: img20,
+        name: "Пицца",
+        type: "pizza",
+    },
+    {
+        src: img21,
+        name: "Акция",
+        type: "sale",
+    },
+];
